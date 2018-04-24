35f3764da5521996d471c42c71689689560f160a
spmaamil <32394113+spmaamil@users.noreply.github.com>
2018-04-24 18:31:22 -0500

    Add files via upload

--- somefinalbs/somefinalbs.ino
+++ somefinalbs/somefinalbs.ino
@@ -1,292 +1,284 @@
 #include <LiquidCrystal.h>
 LiquidCrystal LcdDriver(11,9,5,6,7,8);
-unsigned long timer;
-int curPos;
-boolean board[4][20]; 
-int snake[2];
-int fruitPos[2];
-int snakeState;
-int snakeCount = 0;
-int lives = 3;
-int updateSpeed = 500;
-enum ButtonState { Idle, Wait, Low };
-enum GameState { GameA, GameB };
+unsigned long timer; //keeps track of game time
+int asteroidsPassed; //keeps track of asteroids passed
+int curPos; //keep track of the current position in each game
+boolean board[4][20]; //the game board
+int snake[2]; //location of the snake
+int fruitPos[2]; //location of the fruit for the snake
+int snakeState; //current state of the snake (win, lose, continue)
+int snakeCount; //keeps track of how many fruits the snake as eaten
+int lives; //the current lives for the meteor game
+int updateSpeed; //the current update speed;
+enum ButtonState { Idle, Wait, Low }; //enum for the currrent button state
+enum GameState { GameA, GameB }; //enum for the current game state
 
-long ButtonTime;
+long ButtonTime; //keeps track of how long the button was pressed.
 
-ButtonState BS;
-GameState Game;
-void setup() 
+ButtonState BS; //keeps track of button state
+GameState Game; //keeps track of game state
+void setup() //sets up all needed variables
 {
-  curPos = 0;
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
-  pinMode(4, INPUT);
+  curPos = 0; //sets curpos to 0
+  pinMode(2, INPUT); //sets pin 2 as input
+  pinMode(3, INPUT); //sets pin 3 as input
+  pinMode(4, INPUT); //sets pin 4 as input
   
-  attachInterrupt(0, MonitorA, CHANGE);
-  attachInterrupt(1, MonitorB, CHANGE);
+  attachInterrupt(0, MonitorA, CHANGE); //attaches an interrupt to pin 2
+  attachInterrupt(1, MonitorB, CHANGE); //attaches an interrupt to pin 3
   
+  LcdDriver.begin(20,4); //set board size
+  LcdDriver.clear(); //clears the board
+  LcdDriver.setCursor(0,0);//sets cursor to top left corner
   
-  // put your setup code here, to run once:
-  LcdDriver.begin(20,4);
-  LcdDriver.clear();
-  LcdDriver.setCursor(0,0);
-  
-  
+  BS = Idle; //sets button state to idle
+  Game = GameA; //sets game state to GameA
+  snakeState = 0; //sets snake state to 0
+  snakeCount = 0; //sets snake state to 0
+  lives = 3; //sets lives to three
+  asteroidsPassed = 0; //sets asteriods passed to 0
+  updateSpeed = 500; //sets update speed to 500
 
-  
-  BS = Idle;
-  Game = GameA;
-  snakeState = 0;
+  timer = millis();//sets timer to current time in millis
+  ButtonTime = millis(); //sets button to current time in millis
 
-  timer = millis();
-  ButtonTime = millis();
+} //end setup
 
-}
-
-void MonitorA()
+void MonitorA() // handles interrupts
 {
-  if(digitalRead(2) == digitalRead(3))
-    curPos+=1;
-  else
-    curPos-=1;
-  
-}
+  if(digitalRead(2) == digitalRead(3)) //if states are equal
+    curPos+=1; //add one to cur pos
+  else//states not equal
+    curPos-=1; //remove one from cur pos
+}//end MonitorB
 
-void MonitorB()
+void MonitorB()//handles interrupts
 {
-  if(digitalRead(2) == digitalRead(3))
-    curPos -= 1;
-  else
-    curPos += 1;
-}
+  if(digitalRead(2) == digitalRead(3))//if states are equal
+    curPos -= 1;//remove one from cur pos
+  else//states not equal
+    curPos += 1;//add one to curpos
+}//end MonitorB
 
-void updateGameA()
+void updateGameA()// handles GameA updates
 {  
-  int pos = abs(curPos/4) % 4;
-  boolean temp[4][20];
-  for(int r = 0; r < 4; r++)
+  int pos = abs(curPos/4) % 4;//bounds the pos to a number between 0 and 3
+  boolean temp[4][20];//makes a temporary array to shift
+  for(int r = 0; r < 4; r++)//runs from 0 -> 3
   {
-    for(int k = 20; k > 0; k--)
+    for(int k = 20; k > 0; k--)//runs from 0 -> 19
     {
-      temp[r][k] = board[r][(k - 1) % 20];
-    }
-  }
-  for(int k = 0; k < 4; k++)
+      temp[r][k] = board[r][(k - 1) % 20];//sets the current position with the one infront of it.
+    }//end for
+  }//end for
+  for(int k = 0; k < 4; k++)//runs from 0 -> 3
   {
-    temp[k][0] = false;
-    if(random(0,100) < 20)
-      temp[k][0] = true;
-  }
+    temp[k][0] = false;//sets position to false
+    if(random(0,100) < 20)//randomly adds a position about 20% of the time
+      temp[k][0] = true;//set the position to true
+  }//end for
 
-  for(int r = 0; r < 4; r++)
+  for(int r = 0; r < 4; r++)// runs from 0 -> 3
   {
-    for(int k = 0; k < 20; k++)
+    for(int k = 0; k < 20; k++) //runs from 0 -> 19
     {
-        board[r][k] = temp[r][k];
-    }
-  }
+        board[r][k] = temp[r][k];//copys the state of temp to board
+    }//end for
+  }//end for
+  asteroidsPassed += 1; //adds 1 to asteroids passed
+  if(board[pos][19])//if the curPOs and theres an asteroid are in the same position
+    lives -= 1;//remove one from lives.
+}//end updateGameA
 
-  if(board[pos][19])
-    lives -= 1;
-}
-
-void updateGameB()
+void updateGameB()//handles GameB updates
 {
 
-  int pos = abs(curPos/4) % 4;
+  int pos = abs(curPos/4) % 4;//bounds the pos to a number between 0 and 3
   
-  if(fruitPos[0] == snake[0] && fruitPos[1] == snake[1])
+  if(fruitPos[0] == snake[0] && fruitPos[1] == snake[1]) //if fruitpos is the same as snakepos
   {
-    fruitPos[0] = random(0,4);
-    fruitPos[1] = random(0,20);
-    snakeCount+=1;
-    updateSpeed -= 50;
+    fruitPos[0] = random(0,4);// randomly places on y axis
+    fruitPos[1] = random(0,20);//randomly places on x acis
+    snakeCount+=1;//update snake count 1
+    updateSpeed -= 50;//increments updatespeed
   }
-  if(pos == 0)
+  if(pos == 0)//if facing up
   {
-    snake[0] = snake[0] - 1;
-    snake[1] = snake[1];
-  }
-  else if(pos == 1)
+    snake[0] = snake[0] - 1;//decrement current y pos
+  }//end if
+  else if(pos == 1)//if facing right
   {
-    snake[0] = snake[0];
-    snake[1] = snake[1] - 1;
-  }
-  else if(pos == 2)
+    snake[1] = snake[1] - 1;//decrement x pos
+  }//end if
+  else if(pos == 2)//if facing down
   {
-    snake[0] = snake[0] + 1;
-    snake[1] = snake[1];
-  }
-  else if(pos == 3)
+    snake[0] = snake[0] + 1;//increment y pos
+  }//end if
+  else if(pos == 3)//if facingleft
   {
-    snake[0] = snake[0];
-    snake[1] = snake[1] + 1;
-  }
-  if(snake[0] == -1 || snake[0] == 5)
+    snake[1] = snake[1] + 1;//increment x pos
+  }//end if
+  if(snake[0] == -1 || snake[0] == 5) //if out of y bounds
   {
-    snakeState = 2;
-    return;
-  }
-  else if(snake[1] == -1 || snake[1] == 20)
+    snakeState = 2; //set state to lose
+    return;//exit method
+  }//end if
+  else if(snake[1] == -1 || snake[1] == 20)//if out of x bounds
   {
-    snakeState = 2;
-    return;
-  }
-  if(snakeCount == 10)
+    snakeState = 2;//set state to lose
+    return;//exit method
+  }//end if
+  if(snakeCount == 10)//if collected 10 fruits
   {
-    snakeState = 1;
-    return;
-  }
-}
+    snakeState = 1;//set state to win
+    return;//exit method
+  }//end if
+}// end updateGameB
 
-void drawToLCD()
+void drawToLCD()//Draws to the lcd
 {
-  LcdDriver.clear();
-
+  LcdDriver.clear();//clear the LCD
   
-  if(Game == GameA)
+  if(Game == GameA)//if the current game is GameA
   {
-    int tempPos = (abs(curPos/4) % 4);
-    LcdDriver.setCursor(19,tempPos);
-    LcdDriver.print("<");
-    LcdDriver.setCursor(0,0);
-    LcdDriver.print((char)(lives + 48));
+    int tempPos = (abs(curPos/4) % 4);//bound position from 0 -> 3
+    LcdDriver.setCursor(19,tempPos);//set to position of characted
+    LcdDriver.print("<");//print 
+    LcdDriver.setCursor(0,0);//set cursor to top left
+    LcdDriver.print((char)(lives + 48));//prints the lives
 
-    for(int r = 0; r < 4; r++)
+    for(int r = 0; r < 4; r++)//increment from 0->3
     {
-      for(int k = 0; k < 20; k++)
+      for(int k = 0; k < 20; k++)//increment from 0->19
       {
-          LcdDriver.setCursor(k,r);
-          if(board[r][k])
-            LcdDriver.print("*");
-      }
-    }
-  }
-  else
+          LcdDriver.setCursor(k,r);//sets to current position in board.
+          if(board[r][k])//if the board at this point is true
+            LcdDriver.print("*");//draw an asteroid
+      }//end for
+    }//end for
+  }//end if
+  else//Game == GameB
   {    
-    int tempPos = (abs(curPos/4) % 4);
-    LcdDriver.setCursor(0,0);
-    LcdDriver.print(snakeCount);
-    LcdDriver.setCursor(snake[1],snake[0]);
-    if(tempPos == 0)
-      LcdDriver.print("^");
-    else if(tempPos == 1)
-      LcdDriver.print("<");
-    else if(tempPos == 2)
-      LcdDriver.print("v");
-    else
-      LcdDriver.print(">");
-    LcdDriver.setCursor(fruitPos[1],fruitPos[0]);
-    LcdDriver.print("X");
-  }
-  
-  
-}
+    LcdDriver.setCursor(fruitPos[1],fruitPos[0]);//set position to where the fruit is
+    LcdDriver.print("*");//print at fruit point
+    
+    int tempPos = (abs(curPos/4) % 4);//bounds pos between 0 and 3
+    LcdDriver.setCursor(0,0);// sets cursor to top left
+    LcdDriver.print(snakeCount);//print snake count
+    LcdDriver.setCursor(snake[1],snake[0]);//sets to position of snake
+    if(tempPos == 0)//if facing up
+      LcdDriver.print("^");//print ^
+    else if(tempPos == 1)//if facing right
+      LcdDriver.print("<");//print <
+    else if(tempPos == 2)//if facing down
+      LcdDriver.print("v");//print v
+    else//facing left
+      LcdDriver.print(">");//print >
+    //end if
+  }//end if
+}//end draw to lcd
 
-void ResetGame()
+void ResetGame()//resets everything.
 {
-  lives = 3;
+  lives = 3;//resets 
   for(int r = 0; r < 4; r++)
   {
     for(int k = 0; k < 20; k++)
     {
-        board[r][k] = false;
+        board[r][k] = false;//resets board pos to false
     }
   }
-  
-  timer = millis();
-
-  curPos = 0;
-  snake[0] = 2;
-  snake[1] = 10;
+  asteroidsPassed = 0;//resets asteroids to 0
+  curPos = 0;//resets curpos to 0
+  snake[0] = 2;//resets to middle
+  snake[1] = 10;//resets to middle
         
-  snakeState = 0;
-  snakeCount = 0; 
-  fruitPos[0] = random(0,4);
-  fruitPos[1] = random(0,20);
-  if(Game == GameA)
+  snakeState = 0;//resets to 0
+  snakeCount = 0; //resets to 0 
+  fruitPos[0] = random(0,4);//resets to randomPos
+  fruitPos[1] = random(0,20);//resets to randomPos
+  if(Game == GameA)//is GameA
   {
-    updateSpeed = 500;
+    updateSpeed = 500;//resets to 500
   }
-  else 
+  else //is GameB
   {
-    updateSpeed = 1000;
-  }
-}
+    updateSpeed = 1000;//resets to 1000
+  }//end if
 
-void ChangeGame()
+  timer = millis();//resets to current time
+
+}//End Reset Game
+
+void ChangeGame()//set needed stuff to changegame
 {
-  ResetGame();
-  if(Game == GameA)
+  ResetGame();//calls reset game
+  if(Game == GameA)//If on GameA
   {
-    Game = GameB;
-    updateSpeed = 1000;
+    Game = GameB;//Change to GameB
+    updateSpeed = 1000;//sets update interval to 1000
   }
-  else 
+  else //is GameB
   {
-    Game = GameA;
-    updateSpeed = 500;
-  }
-  
-}
+    Game = GameA;//Change to GameA
+    updateSpeed = 500;//sets update interval to 500
+  }//end if  
+}//end ChangeGame
 
-void loop() 
+void loop() //main method constantly loops
 {
-  int state = ButtonNextState(digitalRead(4));
-  if(state == 2)
-    ResetGame();
-  else if(state == 3)
-    ChangeGame();
-
-  if(GameA == Game)
+  int state = ButtonNextState(digitalRead(4));//gets the current button state
+  if(state == 2)//if short press
+    ResetGame();//call reset game
+  else if(state == 3)//if long press
+    ChangeGame();//call change game
+  if(GameA == Game)//if GameA 
   {
-    if(lives > 0)
+    if(lives > 0)//if lives are greater then 0
     {
-      if(millis() - timer >= updateSpeed)
+      if(millis() - timer >= updateSpeed)//if updateSpeed time has passed
       {
-        updateGameA();
-        drawToLCD();      
-        timer += updateSpeed;
-      }
-  
-    }
-    else if(lives == 0)
+        updateGameA();//call updateGameA method
+        drawToLCD();//call drawToLcd
+        timer += updateSpeed;//update timer
+        if(asteroidsPassed % 19 == 0 && timer - 50 > 250)//if 19 asteroids have updated
+        {
+          asteroidsPassed = 0;//reset them to 0
+          updateSpeed -= 50;//increment update speed
+        }//end if
+      }//end if
+    }//end if
+    else if(lives == 0)//if lives == 0
     {
       //drawToLCD();
-      LcdDriver.setCursor(19,(abs(curPos/4) % 4));
-      LcdDriver.print("X");
-      lives = -1;
-    }
-  }
-  else 
+      LcdDriver.setCursor(19,(abs(curPos/4) % 4));//sets to current pos of person
+      LcdDriver.print("X");//draws an x
+      lives = -1;//sets lives to -1 as not to update constantly
+    }//end if
+  }//end if
+  else //is GameB
   {
-    if(snakeState == 0)
+    if(snakeState == 0)// If haven't lost yet
     {
-      if(millis() - timer >= updateSpeed)
+      if(millis() - timer >= updateSpeed)//if updateSpeed time has passed
       {
-        updateGameB();
-        drawToLCD();      
-        timer += updateSpeed;
-      }
-    }
-    else if(snakeState == 1)
+        updateGameB();//call updateGameB method
+        drawToLCD();// call drawToLcd
+        timer += updateSpeed;//update timer by updateSpeed
+      }//end if
+    }//end if
+    else if(snakeState == 1)//if state == 1
     {
-      LcdDriver.setCursor(0,0);
-      LcdDriver.print("You won!");
-    }
-    else if(snakeState == 2)
+      LcdDriver.setCursor(0,0);//sets cursor to top left
+      LcdDriver.print("You won!");//print you won
+    }//end if
+    else if(snakeState == 2)//if state == 2
     {
-      LcdDriver.setCursor(0,0);
-      LcdDriver.print("You lost!");
-    }
-  }
-  
-  
-  
-  // put your main code here, to run repeatedly:
-  
-}
+      LcdDriver.setCursor(0,0);//sets cursor to top left
+      LcdDriver.print("You lost!");//print you lost
+    }//end if
+  }//end if
+}//end loop
 
 
 int ButtonNextState(int state)//Checks the state of the button
